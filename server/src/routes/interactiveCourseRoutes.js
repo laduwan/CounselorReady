59a98c032b8943fa2fdb3b4014c39c51e053595e
laduwan <126280643+laduwan@users.noreply.github.com>
2026-05-22 21:18:25 -0400

    Update course query to include courseCode field
    Signed-off-by: laduwan <126280643+laduwan@users.noreply.github.com>

--- server/src/routes/interactiveCourseRoutes.js
+++ server/src/routes/interactiveCourseRoutes.js
@@ -203,7 +203,7 @@ router.get('/', async (req, res) => {
     }
 
     const courses = await Course.find(query)
-      .select('title slug description thumbnail ceHours totalEstimatedTime categories tags wordCount sectionCount moduleCount assessmentQuestionCount ceuCategories accessType price pricingTier status ceuHours ceuApprovalNumber')
+      .select('title slug status courseCode description thumbnail ceHours totalEstimatedTime categories tags wordCount sectionCount moduleCount assessmentQuestionCount ceuCategories accessType price pricingTier status ceuHours ceuApprovalNumber')
       .sort({ publishedAt: -1 })
       .skip((page - 1) * limit)
       .limit(parseInt(limit));
